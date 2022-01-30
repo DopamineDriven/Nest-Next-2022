37004c26b931771ce876806511f6b49c9de3a1c2
Andrew Ross <andrew.ross@cortinahealth.com>
2022-01-30 10:55:22 -0600

    lots of graphql tasks, check out the autogenerated schema at ./nest/src/schema.gql

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module, CacheModule } from "@nestjs/common";
-import { GraphQLExecutionContext, GraphQLModule } from "@nestjs/graphql";
+import {
+  GraphQLModule
+} from "@nestjs/graphql";
 import { join } from "path";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import config from "./common/config/config.config";
@@ -8,13 +10,13 @@ import {
   GraphqlConfig
 } from "./common/config/config-interfaces.config";
 import { ExpressContext } from "apollo-server-express";
-import {
-  Context as ApolloContext,
-  ApolloServerPluginLandingPageLocalDefault,
-  ApolloServerPluginLandingPageGraphQLPlaygroundOptions,
-  ApolloServerPluginLandingPageGraphQLPlayground,
-  ApolloServerPluginInlineTrace
-} from "apollo-server-core";
+// import {
+//   Context as ApolloContext,
+//   ApolloServerPluginLandingPageLocalDefault,
+//   ApolloServerPluginLandingPageGraphQLPlaygroundOptions,
+//   ApolloServerPluginLandingPageGraphQLPlayground,
+//   ApolloServerPluginInlineTrace
+// } from "apollo-server-core";
 import { AppController } from "./app/app.controller";
 import { AppService } from "./app/app.service";
 import { AppResolver } from "./app/app.resolver";
@@ -27,16 +29,30 @@ import { PasswordModule } from "./password/password.module";
 import { AuthJwtModule } from "./auth/auth-jwt.module";
 import { UserModule } from "./user/user.module";
 import { PaginationModule } from "./pagination/pagination.module";
-import { PrismaService } from "./prisma/prisma.service";
-import { GraphQLResolveInfo, lexicographicSortSchema } from "graphql";
-import { User } from "./user/model/user.model";
+// import { PrismaService } from "./prisma/prisma.service";
+// import { GraphQLError, GraphQLResolveInfo, lexicographicSortSchema } from "graphql";
+// import { User } from "./user/model/user.model";
+// import * as ApolloServerOperationRegistery from "apollo-server-plugin-operation-registry/src/index";
 import { EntryModule } from "./entry/entry.module";
+// import {
+//   ClientRequest,
+//   IncomingMessage,
+//   RequestOptions,
+//   ServerResponse
+// } from "http";
+// import { URL } from "url";
+// import {
+//   GraphQLRequest,
+//   GraphQLRequestContext,
+//   GraphQLResponse,
+//   GraphQLRequestContextExecutionDidStart
+// } from "apollo-server-types";
+// import { UnwrapPromise } from "@prisma/client";
+// import { Request, Response } from "express";
 
-export type Context = {
-  req?: ExpressContext["req"];
-  token: string | null;
-  res?: ExpressContext["res"];
-};
+
+
+export type Context = { req: ExpressContext['req'], res: ExpressContext['res'], token: string | null };
 @Module({
   imports: [
     CacheModule.register({
@@ -48,7 +64,9 @@ export type Context = {
       envFilePath: "./env"
     }),
     GraphQLModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => {
+      useFactory: async (
+        configService: ConfigService
+       ) => {
         const rootSchema = await loadSchema("./src/schema.gql", {
           loaders: [new GraphQLFileLoader()]
         });
@@ -79,36 +97,42 @@ export type Context = {
             key: apolloConfig?.key ? apolloConfig.key : ""
           },
           schema: rootSchema,
-          playground: {              env: ".env",
-          settings: {
-            "general.betaUpdates": true,
-            "tracing.hideTracingResponse": false,
-            "request.credentials": "include",
-            "schema.polling.interval": 5000
-          }},
+          playground: {
+            env: ".env",
+            settings: {
+              "general.betaUpdates": true,
+              "tracing.hideTracingResponse": false,
+              "request.credentials": "include",
+              "schema.polling.interval": 5000
+            }
+          },
           // validationRules: [
           //   (context: ValidationContext) => [context.getSchema()]
           // ],
+          plugins: [
+            (await import("apollo-server-plugin-operation-registry")).default({
+              debug: true
+            })
+          ],
           // plugins: [
           //   ApolloServerPluginLandingPageLocalDefault(),
           //   ApolloServerPluginInlineTrace()
           // ],
           debug: graphqlConfig?.debug
-          ? graphqlConfig.debug
-          : process.env.NODE_ENV !== "production"
-          ? true
-          : false,
-          context: ({ req, res }: ExpressContext): any => {
-            const token = req.headers.authorization;
+            ? graphqlConfig.debug
+            : process.env.NODE_ENV !== "production"
+            ? true
+            : false,
+          context: async ({ req, res }: ExpressContext): Promise<Context>=> {
+            const token = req.header("authorization");
+            const ctx = {req, res, token: token || null}
             if (token != undefined) {
               console.log(token ?? "still no token");
               return { token: token, req, res };
             } else {
-              return {
-                req,
-                res
-              };
+              return {req, res, token: null}
             }
+            
           }
         };
       },
@@ -132,3 +156,12 @@ export type Context = {
   ]
 })
 export class AppModule {}
+
+/**
+ * { 
+          ...props
+        }: GqlModuleOptions
+      ): Promise<GqlModuleOptions
+ * requestDidStart: (requestContext: GraphQLRequestContext<Context>) => ([requestContext.request.http?.headers.get("authorization")])})
+GraphQLRequestContextExecutionDidStart<Context> 
+*/
