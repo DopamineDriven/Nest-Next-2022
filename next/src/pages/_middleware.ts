4feb5b5ad19d9f1e87b39eb8d3027a94ea3eef60
Andrew Ross <andrew.ross@cortinahealth.com>
2022-02-15 17:24:58 -0600

    implemented -- pushing Nest to ECR and Next to Vercel in days to come

--- next/src/pages/_middleware.ts
+++ next/src/pages/_middleware.ts
@@ -1,22 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { INTERNALS } from "next/dist/server/web/spec-extension/request";
 import cors, { CorsOptions } from "@/lib/cors";
-import { WebNextResponse } from "next/dist/server/base-http";
 
 // (next: () => NextResponse)
 
 export default async function middleware(req: NextRequest) {
   req.cookies;
-  console.log(JSON.stringify(req.cookies ?? {}, null, 2));
-  if (req.headers.get("authorization") && req.headers.get("authorization") != null) {
-    return WebNextResponse.prototype.setHeader("authorization", `Bearer ${(req.headers.get("authorization")?.split(/([ ])/)[1])?.trim()}`)
+  console.log(req.cookies ?? "no cooks");
+  if (
+    req.headers.get("authorization") &&
+    req.headers.get("authorization") != null
+  ) {
+    return NextResponse.prototype.headers.set(
+      "authorization",
+      `Bearer ${req.headers
+        .get("authorization")
+        ?.split(/([ ])/)[1]
+        ?.trim()}`
+    );
   }
-  if (req.nextUrl.pathname === "/") {
-    const getCookieFromSignupOrloginMutations = JSON.parse(req.cookies['nest-to-next-2022' || "false"]);
-    console.log(getCookieFromSignupOrloginMutations ?? "no cookies at the edge");
-    return NextResponse.rewrite(`/${getCookieFromSignupOrloginMutations ? '/profile' : "/"}`);
-  }
+  // if (req.nextUrl.pathname === "/" || "") {
+  //   const getCookieFromSignupOrloginMutations = JSON.parse(
+  //     req.cookies["nest-to-next-2022" || "false"]
+  //   );
+  //   return NextResponse.rewrite(
+  //     `/${getCookieFromSignupOrloginMutations ? "profile" : "index"}`
+  //   );
+  // }
   const response = NextResponse.next();
   response.headers.set("Referrer-Policy", "Origin-When-Cross-Origin");
   response.headers.set(
